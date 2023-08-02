07e322fc1a73dec4117bc25d03e5179e3a81927d
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-08-02 13:33:40 +0200

    api hit_ post and pull

---
+++ src/api.js
@@ -0,0 +1,105 @@
+// fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({
+//     name: 'Soccer LeadorBoard'
+//   })
+// })
+//   .then(response => response.json())
+//   .then(data => {
+//     const uniqueID = data.result.match(/ID: (\w+)/)[1]; // Extracting the ID from the response using regex
+//     console.log('Received unique ID:', uniqueID);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
+
+
+const form = document.getElementById('form');
+form.addEventListener('submit', submitForm);
+let gameId = ''
+
+function submitForm(event) {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
+
+  const data = {
+    name: nameInput.value,
+    score: scoreInput.value,
+    user: 'John Doe' 
+  };
+
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      name: 'Soccer LeadorBoard'
+    })
+  })
+  .then(response => response.json())
+  .then(data => {
+    const id = data.result.match(/ID: (\w+)/)[1];
+    gameId = id; 
+    return id;
+  })
+  .then(id => {
+      fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+        .then(response => response.json())
+        .then(result => {
+          console.log('Score posted successfully:', result);
+        })
+        .catch(error => {
+          console.error('Error posting score:', error);
+          return alert('something went wrong');
+        });
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+
+    // display data
+
+const dynamicDisplay = document.getElementById('dynamicDisplay');
+const refreshButton = document.getElementById('refresh')
+
+
+const refreshScores = () => {
+    const id = gameId; // Assign the gameId value to the id variable
+  
+    fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`)
+      .then(response => response.json())
+      .then(data => {
+        console.log('Received scores:', data);
+        displayScores(data.result);
+      })
+      .catch(error => {
+        console.error('Error fetching scores:', error);
+      });
+  }
+
+
+  function displayScores(scores) {
+    dynamicDisplay.innerHTML = ''; 
+
+    scores.forEach(score => {
+      const scoreElement = document.createElement('div');
+      scoreElement.innerHTML = `${score.user}: ${score.score}`;
+      dynamicDisplay.appendChild(scoreElement);
+    });
+  }
+
+refreshButton.addEventListener('click', refreshScores);
