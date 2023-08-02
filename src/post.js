8cac6fb4de6327fa9e551041c93a4a9e95bfe9fa
Shaun K <123934813+paulshaun-kdtk@users.noreply.github.com>
2023-08-02 10:10:02 +0200

    Linter add Readme Update

--- src/post.js
+++ src/post.js
@@ -1,13 +1,13 @@
 function addContent() {
-    const name = document.getElementById("name").value;
-    const score = document.getElementById("score").value;
-  
-    const newContent = document.createElement("div");
-    newContent.innerHTML = `<p>${name}: ${score}</p>`;
-  
-    const dynamicDisplay = document.getElementById("dynamicDisplay");
-    dynamicDisplay.appendChild(newContent);
-  }
+  const name = document.getElementById('name').value;
+  const score = document.getElementById('score').value;
+
+  const newContent = document.createElement('div');
+  newContent.innerHTML = `<p>${name}: ${score}</p>`;
+
+  const dynamicDisplay = document.getElementById('dynamicDisplay');
+  dynamicDisplay.appendChild(newContent);
+}
 
 const submitButton = document.querySelector('input[type="button"]');
-submitButton.addEventListener("click", addContent);
+submitButton.addEventListener('click', addContent);
